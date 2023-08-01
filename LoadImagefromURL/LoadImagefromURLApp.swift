5648f97049327b6804ce1ca8c48af6f79d1c07e3
Pratik Pandya <pratik.pandya.official@gmail.com>
2023-08-01 16:10:11 +0530

    Image load with URL

---
+++ LoadImagefromURL/LoadImagefromURLApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadImagefromURLApp.swift
+//  LoadImagefromURL
+//
+//  Created by Drashti on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LoadImagefromURLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
